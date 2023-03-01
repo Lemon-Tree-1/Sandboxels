6d707da0bfd85aaeeed781794d3b1e62b1e452c6
Lemon-Tree-1 <126728968+Lemon-Tree-1@users.noreply.github.com>
2023-03-01 18:25:52 -0500

    Create LemonAlchemy.js

---
+++ LemonAlchemy.js
@@ -0,0 +1,29 @@
+// Warning: This mod will remove all of your previously unlocked elements.
+
+// loop through the elements object
+if (elements.explosion) {
+    elements.explosion.category = "tools";
+}
+for (var element in elements) {
+    if (elements[element].category !== "tools") {
+        // give the element the hidden attribute true
+        elements[element].hidden = true;
+        // set its category to "alchemy mod"
+        elements[element].category = "alchemy mod";
+    }
+}
+
+// set the unhide setting to Unlock as Discovered (2)
+settings.unhide = 2;
+
+// unhide magma, hydrogen, oxygen, nitrogen, and carbon dioxide
+if (!settings.unlocked.alchemymod) {
+    settings.unlocked = {
+        "magma": true,
+        "hydrogen": true,
+        "oxygen": true,
+        "nitrogen": true,
+        "carbon_dioxide": true,
+        "alchemymod": true,
+    };
+}
